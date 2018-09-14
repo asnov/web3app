abf55e35703e0c560bda009a129a82bc8997019a
Alex Novikov <alex@asnovikov.ru>
2018-09-14 01:35:00 -0300

    replaced web3 with ethers - Uncaught Error: Cannot find module 'tslib';

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,15 @@
 import { Component } from '@angular/core';
 
-import * as Web3 from 'web3';
-import BigNumber from 'web3/bower/bignumber.js/bignumber';
+// import * as Web3Provider from 'web3';
+// import BigNumber from 'web3/bower/bignumber.js/bignumber';
+
+// import { BigNumber } from 'ethers';
+// import { JsonRpcProvider, Web3Provider } from 'ethers/providers';
+import * as ethers from 'ethers';
+import BigNumber = ethers.BigNumber;
+import JsonRpcProvider = ethers.providers.JsonRpcProvider;
+import Web3Provider = ethers.providers.Web3Provider;
+
 
 interface Web3broserWindow extends Window {
   web3?: {
@@ -10,7 +18,7 @@ interface Web3broserWindow extends Window {
 }
 
 interface AccountInfo {
-  fromAccount: string;
+  account: string;
   balance: BigNumber;
 }
 
@@ -26,7 +34,7 @@ export class AppComponent {
 
   title = 'web3app';
 
-  private web3Provider: null;
+  private web3Provider: any;
   private web3: {
     eth: {
       getCoinbase: (callback: (err, account: string) => void) => void;
@@ -34,17 +42,21 @@ export class AppComponent {
     }
     fromWei: Function;
   };
+  private ethers: Web3Provider;
   accountInfo: AccountInfo;
 
   constructor() {
     if (typeof window.web3 !== 'undefined') {
       this.web3Provider = window.web3.currentProvider;
     } else {
-      this.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
+      // this.web3Provider = new Web3Provider.providers.HttpProvider('http://localhost:8545');
+      this.web3Provider = new JsonRpcProvider('http://localhost:8545');
     }
-    this.web3 = new Web3(this.web3Provider);
+    // this.web3 = new Web3Provider(this.web3Provider);
+    this.ethers = new Web3Provider(this.web3Provider);
 
-    this.getAccountInfo()
+    // this.getAccountInfo()
+    this.getAccountInfoEthers()
       .then(accountInfo => {
         console.log(`accountInfo=`, accountInfo);
         this.accountInfo = accountInfo;
@@ -60,7 +72,7 @@ export class AppComponent {
           this.web3.eth.getBalance(account, (errBalance, balance) => {
             if (errBalance === null) {
               console.log(`web3.eth.getBalance():`, balance);
-              resolve({fromAccount: account, balance: this.web3.fromWei(balance, 'ether')});
+              resolve({account, balance: this.web3.fromWei(balance, 'ether')});
             } else {
               console.error(`Error: web3.eth.getBalance:`, errCoinbase);
               reject(`Error: ${errBalance}`);
@@ -71,6 +83,28 @@ export class AppComponent {
           reject(`Error: ${errCoinbase}`);
         }
       });
+    });
+  }
+
+  getAccountInfoEthers(): Promise<AccountInfo> {
+    return new Promise<AccountInfo>((resolve, reject) => {
+      this.ethers.listAccounts()
+        .then(value => {
+          console.log(`provider.listAccounts() returns`, value);
+          const account = value[0];
+          this.ethers.getBalance(account)
+            .then(balance => {
+              resolve({account, balance});
+            })
+            .catch(err => {
+              console.error(`provider.getBalance() error is`, err);
+              reject(err);
+            });
+        })
+        .catch(err => {
+          console.error(`provider.listAccounts() error is`, err);
+          reject(err);
+        });
     });
   }
 
